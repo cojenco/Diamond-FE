1c8a240c45756aa316149ae6d71a5fbea9017af9
cojenco <icathyouyang@gmail.com>
2020-07-09 21:47:37 -0700

    initial testing to render Google Maps JS API via react-google-maps package

--- trails-live/src/App.js
+++ trails-live/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 47.62, lng: -122.19 }}
+    />
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+// function App() {
+//   return (
+//     <div style={{ width: '100vw', height: '100vh'}} >
+//     <WrappedMap 
+//       googleMapURL= {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+//       loadingElement={<div style={{ height: "100%" }} />}
+//       containerElement={<div style={{ height: "100%" }} />}
+//       mapElement={<div style={{ height: "100%" }} />}
+//     />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Trails LIVE
     </div>
   );
 }
